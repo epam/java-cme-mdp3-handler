38fd32ea7a56be0427680be47fe079c4f65461f4
Steven Warwick <swarwick@tibco.com>
2019-05-28 16:32:10 -0400

    Updated test which sets max TCP Attempts to 1 and then tries 2 TCP recovery attempts to test that the number of attempts is reset to 0 after successful.

--- mbp-with-mbo/src/test/java/com/epam/cme/mdp3/test/control/GapChannelControllerTest.java
+++ mbp-with-mbo/src/test/java/com/epam/cme/mdp3/test/control/GapChannelControllerTest.java
@@ -194,10 +194,11 @@ public class GapChannelControllerTest {
         Buffer<MdpPacket> buffer = new MDPOffHeapBuffer(bufferCapacity);
         testRecoveryManager = new TestSnapshotRecoveryManager();
         int gapThreshold = 3;
+        int maxTCPAttempts = 1; // set to one to allow second attempt to test reset of number of TCP attempts after valid recovery
         OffHeapSnapshotCycleHandler cycleHandler = new OffHeapSnapshotCycleHandler();
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         gapChannelController = new GapChannelController(Collections.singletonList(testChannelListener),
-                testChannelController, testChannelController, testRecoveryManager, buffer, gapThreshold, GapChannelController.MAX_NUMBER_OF_TCP_ATTEMPTS, testChannelId,
+                testChannelController, testChannelController, testRecoveryManager, buffer, gapThreshold, maxTCPAttempts, testChannelId,
                 mdpMessageTypes, cycleHandler, cycleHandler, executorService, new TestTCPMessageRequester(), null, null);
 
 
@@ -216,6 +217,21 @@ public class GapChannelControllerTest {
             assertEquals(i, nextIncrementalMessagePair.getRight().getMsgSeqNum());
         }
 
+        lastMsgSeqNumProcessed = 7;
+        sendInitialMBOSnapshot(lastMsgSeqNumProcessed);
+        
+        gapChannelController.handleIncrementalPacket(incrementContext, createPacketWithIncrement(8));
+        gapChannelController.handleIncrementalPacket(incrementContext, createPacketWithIncrement(13));
+        assertFalse(snapshotRecoveryStarted);
+        gapChannelController.handleIncrementalPacket(incrementContext, createPacketWithIncrement(10));
+        gapChannelController.handleIncrementalPacket(incrementContext, createPacketWithIncrement(14));
+
+        for(int i = 8; i <= 14; i++){
+            Pair<MdpFeedContext, MdpPacket> nextIncrementalMessagePair = testChannelController.nextIncrementalMessage();
+            assertNotNull("messages with sequence " + i + " has not been received", nextIncrementalMessagePair);
+            assertEquals(i, nextIncrementalMessagePair.getRight().getMsgSeqNum());
+        }
+        
         executorService.shutdownNow();
     }
 
